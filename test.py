4367d759cee845ebfdd8fd980f15791822093486
Lennart Schulze <schulzelennart@gmail.com>
2023-01-29 02:51:34 -0500

    quantum major update

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
 from pipelines.pic2num import avg_color
 from quantum_driver import get_quantum_parameters
 
+dict = {"v_"}
+avgs = []
 file = "input_data/flower.jpg"
-avg = avg_color(file)
-print(avg)
-out_params = get_quantum_parameters(2,{0:avg})
+avgs.append(avg_color(file))
+file = "input_data/flower2.webp"
+avgs.append(avg_color(file))
+print(avgs)
+out_params = get_quantum_parameters(2,[[0,1], [1,2]], [[2,3], [3,4]])
 print(out_params)
