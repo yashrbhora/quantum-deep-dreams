d9f388872aaffc84e23f61f1471f20bef69a45dd
Lennart Schulze <schulzelennart@gmail.com>
2023-01-29 03:19:35 -0500

    update integration with vid2num

--- test.py
+++ test.py
@@ -1,12 +1,16 @@
 from pipelines.pic2num import avg_color
+from pipelines.vid2num import batch_process
 from quantum_driver import get_quantum_parameters
 
 dict = {"v_"}
 avgs = []
 file = "input_data/flower.jpg"
-avgs.append(avg_color(file))
+#avgs.append(avg_color(file))
 file = "input_data/flower2.webp"
-avgs.append(avg_color(file))
-print(avgs)
+#avgs.append(avg_color(file))
+
+result = batch_process("videos")
+print(result)
+#print(avgs)
 out_params = get_quantum_parameters(2,[[0,1], [1,2]], [[2,3], [3,4]])
 print(out_params)
